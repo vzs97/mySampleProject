8b4c06d9695fab790ab83a52e950ed48bb17b77a
vzs97 <vzs97@163.com>
2022-05-08 20:00:36 +0800

    Remove cassandra sample

--- src/main/java/com/vzs/mysampleproject/repository/cassandra/GreetingsRepository.java
+++ src/main/java/com/vzs/mysampleproject/repository/cassandra/GreetingsRepository.java
@@ -1,19 +1,21 @@
 package com.vzs.mysampleproject.repository.cassandra;
 
 import com.vzs.mysampleproject.entity.cassandra.Greeting;
-import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.data.cassandra.repository.Query;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+//import org.springframework.data.cassandra.repository.CassandraRepository;
+//import org.springframework.data.cassandra.repository.Query;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.domain.Slice;
 
 import java.util.UUID;
 
-public interface GreetingsRepository extends CassandraRepository<Greeting, UUID> {
+public interface GreetingsRepository
+//        extends CassandraRepository<Greeting, UUID>
+{
  
-    Slice<Greeting> findByUser(String user, Pageable pageable) ;
-
-    @Query("SELECT*FROM greetings WHERE user=?0 AND id<?1 LIMIT ?2")
-    Iterable<Greeting> findByUserFrom(String user, UUID from, Integer limit);
-
-    Greeting findByIdAndUser(UUID id, String user);
+//    Slice<Greeting> findByUser(String user, Pageable pageable) ;
+//
+//    @Query("SELECT*FROM greetings WHERE user=?0 AND id<?1 LIMIT ?2")
+//    Iterable<Greeting> findByUserFrom(String user, UUID from, Integer limit);
+//
+//    Greeting findByIdAndUser(UUID id, String user);
 }
